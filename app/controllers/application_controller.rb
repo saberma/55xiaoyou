6af4340523f58fc520ea3eea27b9a6b1bc8f48fa
po <mahb45gmail.com>
2009-09-14 16:22:04 +0800

    add home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,13 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  before_filter :init_title
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  def init_title
+    @title = "小游网 | 在线小游戏,一个接一个,简单,好玩"
+  end
+
 end
