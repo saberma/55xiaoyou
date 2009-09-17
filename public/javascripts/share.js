027f2e816b7e680f011ba34ee060fb7ea786fc62
po <mahb45gmail.com>
2009-09-17 22:00:22 +0800

    加入share

---
+++ public/javascripts/share.js
@@ -0,0 +1,35 @@
+//收藏分享
+var Share = {
+
+  douban: function(url,title){
+    alert('in');
+    var u='http://www.douban.com/recommend/?url='+url+'&title='+encodeURIComponent(title);
+    window.open(u,'douban','toolbar=0,resizable=1,scrollbars=yes,status=1,width=450,height=330');
+    void(0);
+  },
+
+  yahoo: function(url,title){
+    var u='http://myweb.cn.yahoo.com/popadd.html?url='+url+'&title='+encodeURIComponent(title);
+    window.open(u,'yahoo','toolbar=0,resizable=1,scrollbars=yes,status=1,width=450,height=430');
+    void(0);
+  },
+
+  baidu: function(url,title){
+    var u='http://cang.baidu.com/do/add?c=&fr=ien#nw=1&iu='+url+'&it='+encodeURIComponent(title);
+    window.open(u,'baidu','toolbar=0,resizable=1,scrollbars=yes,status=1,width=450,height=430');
+    void(0);
+  },
+
+  qq: function(url,title){
+    var u='http://shuqian.qq.com/post?from=3&jumpback=2&noui=1&uri='+url+'&title='+encodeURIComponent(title);
+    window.open(u,'qq','toolbar=0,resizable=1,scrollbars=yes,status=1,width=450,height=430');
+    void(0);
+  },
+
+  xiaonei: function(url,title){
+    var u='http://share.xiaonei.com/share/buttonshare.do?link='+url+'&title='+encodeURIComponent(title);
+    window.open(u,'xiaonei','toolbar=0,resizable=1,scrollbars=yes,status=1,width=450,height=430');
+    void(0);
+  }
+
+}
