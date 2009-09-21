f82dcc2acacc1f808b47d7c3e5ba94fd9ed2b9b9
saberma <mahb45@gmail.com>
2009-09-21 20:45:54 +0800

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,39 @@
+require 'deprec'
+
+set :domain, "55xiaoyou.com"
+set :application, "55xiaoyou"
+set :repository,  "git://github.com/saberma/55xiaoyou.git"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/var/www/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+set :deploy_via, :remote_cache
+set :git_shallow_clone, 1
+
+set :ruby_vm_type, :mri
+set :web_server_type, :nginx
+set :app_server_type, :mongrel
+set :db_server_type, :mysql
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+set :database_yml_in_scm, false
+
+set :user, "saberma"
+set :runner, nil
+
+set :packages_for_project, %w(libxslt1-dev libxml2-dev imagemagick memcached)
+set :gems_for_project, %w(rspec hoe haml mislav-will_paginate mysql cucumber webrat request-log-analyzer)
+#set :shared_dirs, %w(public/photos)
+
+namespace :deploy do
+  task :restart, :roles => :app, :except => {:no_release => true } do
+    top.deprec.app.restart
+  end
+end
