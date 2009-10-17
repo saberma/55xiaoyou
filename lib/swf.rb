520726f4f196c755abb933f295dd3fffaca8bed5
saberma <mahb45@gmail.com>
2009-10-17 22:02:30 +0800

    some swf file will broken if it was download

--- lib/swf.rb
+++ lib/swf.rb
@@ -1,17 +1,19 @@
 class Swf
   def self.get
     Game.all(:conditions => ["url like ?", "http%"]).each do |g|
-      url = URI.parse(g.url).path
-      filename = url[(url.rindex('/')+1)..url.size]
-      full_filename = File.join(RAILS_ROOT, "public", "swfs", filename)
+      unless g.url =~ /#remote$/
+        url = URI.parse(g.url).path
+        filename = url[(url.rindex('/')+1)..url.size]
+        full_filename = File.join(RAILS_ROOT, "public", "swfs", filename)
 
-      if !File.exists?(full_filename)
-        system("wget #{g.url} -O #{full_filename}") 
-      else
-        p "File #{g.url} exist!"
+        if !File.exists?(full_filename)
+          system("wget #{g.url} -O #{full_filename}") 
+        else
+          p "File #{g.url} exist!"
+        end
+
+        g.update_attribute :url, "/swfs/#{filename}"
       end
-
-      g.update_attribute :url, "/swfs/#{filename}"
     end
     nil
   end
