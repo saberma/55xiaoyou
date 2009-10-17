3fae15f893d89d0673f807f33dc646d6e06c3423
saberma <mahb45@gmail.com>
2009-10-17 21:47:42 +0800

    homepage add latest game

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,6 @@ class HomeController < ApplicationController
   def index
     @popular_list = Game.popular
     @now_list =  Rails.cache.fetch("now_list"){ Game.limit(5).popular }.take(4)
+    @latest_list =  Game.limit(4)
   end
 end
